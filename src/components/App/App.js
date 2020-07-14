f486f7e5cd2cbb38bc8d2d9eca2e3e971c03b959
Kenneth Carter <kxccarter@gmail.com>
2020-07-14 11:35:18 -0500

    Complete Challenge

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,15 +5,19 @@ import Sensors from '../Sensors/Sensors';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className='dashboard'>
-        <Header />
-        <RocketStatus />
-        <Sensors />
-      </div>
-    );
-  }
+    render() {
+        return ( <
+            div className = 'dashboard' >
+            <
+            Header / >
+            <
+            RocketStatus / >
+            <
+            Sensors / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
